45f113c221da471230e3227fad2e0199bfb6bcb2
JayTailor45 <tailorj64@gmail.com>
2019-11-10 18:55:25 +0530

    DAY 1

--- absoluteValuesSumMinization/absoluteValuesSumMinization.ts
+++ absoluteValuesSumMinization/absoluteValuesSumMinization.ts
@@ -1,8 +1,9 @@
 function absoluteValuesSumMinimization(a: number[]): number {
-
+    const finalIndex = a.length % 2 == 0 ? (a.length / 2) - 1 : a.length / 2;  
+    return a[Math.floor(finalIndex)];
 }
 
-// console.log(absoluteValuesSumMinimization([2, 4, 7]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
+console.log(absoluteValuesSumMinimization([2, 4, 7]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
